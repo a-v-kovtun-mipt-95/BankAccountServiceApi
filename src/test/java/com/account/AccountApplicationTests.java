97533b6f8494c18f21aaf1768c7931fe4119f366
Andrii Kovtun <a.v.kovtun.mipt.95@gmail.com>
2022-09-04 19:29:11 +0200

    test fix

--- src/test/java/com/account/AccountApplicationTests.java
+++ src/test/java/com/account/AccountApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AccountApplicationTests {
 
 	@Test
 	void contextLoads() {
