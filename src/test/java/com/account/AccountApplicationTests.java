0725339c577088f8f8c159d937a9cc9687c8f373
Andrii Kovtun <a.v.kovtun.mipt.95@gmail.com>
2022-09-05 08:50:04 +0200

    tests

--- src/test/java/com/account/AccountApplicationTests.java
+++ src/test/java/com/account/AccountApplicationTests.java
@@ -1,13 +1,33 @@
 package com.account;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
 
 @SpringBootTest
 class AccountApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void givenAccountDoesNotExists_whenRequestCheckAccount_then406IsReceived() throws IOException {
+	
+		// Given
+		String name = RandomStringUtils.randomAlphabetic( 8 );
+		HttpUriRequest request = new HttpGet( "http://localhost:8080/check?account=" + name );
+
+		// When
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+
+		// Then
+		assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_NOT_ACCEPTABLE);
 	}
 
 }
